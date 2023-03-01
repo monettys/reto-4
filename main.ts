228952a16871bfa209b1413de5c1609021c8c26a
monettys <124165786+monettys@users.noreply.github.com>
2023-03-01 14:56:27 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.clearScreen()
+basic.forever(function () {
+    if (0 == 0) {
+    	
+    } else if (0 == 0) {
+    	
+    } else if (0 == 0) {
+    	
+    } else if (0 == 0) {
+    	
+    } else {
+    	
+    }
+})
